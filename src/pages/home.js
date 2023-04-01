1050643f842d19dcdeb17a610d22ae28d4db4736
WegaSilver <109556907+TonyBoyDeFi@users.noreply.github.com>
2023-04-01 12:41:59 +0300

    Content

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,13 +9,8 @@ const Home = () => {
         <s.TextDescription fs={"89px"}>AscensionOne</s.TextDescription>
         <s.TextDescription style={{ textAlign: "center" }}>
           Safe and secure launchpad for verified projects #clicksafe
-          <div><<div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="" alt="Card image cap">
-          <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-          </div>
-          0$</div>
+          <br/>
+          Content to be added very soon!
         </s.TextDescription>
         <s.SpacerLarge />
         <s.TextDescription>{}</s.TextDescription>
