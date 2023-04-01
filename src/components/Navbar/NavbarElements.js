45d26acb63cb9eb21428400ad2e63ee22039d32d
WegaSilver <109556907+TonyBoyDeFi@users.noreply.github.com>
2023-04-01 11:12:11 +0300

    Update NavbarElements.js

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -3,7 +3,7 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-    height: 60px;
+    height: 65px;
     display: flex;
     padding: 10px;
     align-items: center;
